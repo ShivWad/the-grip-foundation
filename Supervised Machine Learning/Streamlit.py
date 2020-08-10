0f572f0ced29aca2b4b9059c4054688714808088
ShivWad <shivamwadhawan2000@gmail.com>
2020-08-10 18:08:01 +0530

    TASK

---
+++ Supervised Machine Learning/Streamlit.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pickle as pkl
+
+pickle_in = open('regressor.pickle', 'rb')
+model = pkl.load(pickle_in)
+
+pickle_in.close()
+st.title('SCORE PREDICTION APP')
+st.header('ADD HOURS OF STUDY BELOW')
+
+hours = st.number_input("ENTER HOURS")
+
+st.success("PREDICTED SCORE OF THE STUDENT: {}".format(model.predict([[hours]])))
